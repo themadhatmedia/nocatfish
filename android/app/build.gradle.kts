736fa89d09d850903bc3b747d763efc2fbc85587
JAYTARPARA <tarparajay00@gmail.com>
2026-01-26 17:37:28 +0530

    changed package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.nocatfish.app"
+    namespace = "com.nocatfishscan.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.nocatfish.app"
+        applicationId = "com.nocatfishscan.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/nocatfishscan/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/nocatfishscan/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.nocatfish.app
+package com.nocatfishscan.app
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
